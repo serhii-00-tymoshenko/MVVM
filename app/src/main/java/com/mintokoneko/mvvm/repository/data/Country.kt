0bdeadecb7b20c050324c24ddcd6bfa5423b542c
Serhii Tymoshenko <serhii.tymoshenko.ua@gmail.com>
2023-12-07 14:44:24 +0100

    data

---
+++ app/src/main/java/com/mintokoneko/mvvm/repository/data/Country.kt
@@ -0,0 +1,6 @@
+package com.mintokoneko.helloworld.repository.data
+
+data class Country(
+    val name: String,
+    val id: Int
+)
---
+++ app/src/main/java/com/mintokoneko/mvvm/repository/data/Credentials.kt
@@ -0,0 +1,6 @@
+package com.mintokoneko.helloworld.repository.data
+
+data class Credentials(
+    val login: String,
+    val password: String
+)
